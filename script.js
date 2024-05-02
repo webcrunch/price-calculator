096aef2a52d025dc27087df7f57c36abdb7d53b8
Jarl Lindquist <jarllindquist@gmail.com>
2024-05-02 14:33:42 +0200

    install WPA test...

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
 let total = 0;
 let priceArray = []
 const totalElement = document.getElementById('total');
 const itemList = document.getElementById('item-list');
+const paymentElement = document.getElementById('customerPayment');
+const changeElement = document.getElementById('change');
 
 document.querySelectorAll('.button').forEach(button => {
     button.addEventListener('click', event => {
@@ -31,6 +34,33 @@ const displayItemList = () => {
 
 }
 
+
+document.getElementById('order').addEventListener('click', () => {
+    const savedData = JSON.parse(localStorage.getItem('OrderHistory'));
+    const totalPrice = priceArray.reduce((sum, item) => sum + Number(item.price), 0);
+    const savedArrayData = [{ "totalPrice": totalPrice, "items": priceArray }]
+
+
+    let addOrderHistoryArry;
+    savedData !== null ? addOrderHistoryArry = [...savedArrayData, savedData] : addOrderHistoryArry = savedArrayData
+    localStorage.setItem('OrderHistory', JSON.stringify(addOrderHistoryArry));
+    priceArray = []
+    displayItemList()
+
+});
+
+
+paymentElement.addEventListener('input', function () {
+    // Hämta värdena
+    const totalPrice = priceArray.reduce((sum, item) => sum + Number(item.price), 0);
+    let payment = parseInt(paymentElement.value);
+
+    // Utför beräkningen
+    let change = payment - totalPrice < 0 ? 0 : payment - totalPrice;
+
+    // Uppdatera texten i det andra elementet
+    changeElement.textContent = 'Payment back to customer: ' + change + ' ₱';
+});
 
 document.getElementById('clear').addEventListener('click', () => {
     priceArray = []
