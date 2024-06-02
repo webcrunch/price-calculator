d25e29aad7aef6c18264d71a4770270ec6a3cd57
webcrunch <jarllindquist@gmail.com>
2024-06-02 02:07:41 +0200

    fixing somem navigation design add,update,delete menu. add a test.css to have later on maybe

--- scripts/admin.mjs
+++ scripts/admin.mjs
@@ -35,6 +35,7 @@ const buttonContainer = document.getElementById('button-container');
 // }
 
 const displayButtons = () => {
+    buttonContainer.innerHTML = ""
     // Loopa igenom arrayen
     const buttons = JSON.parse(localStorage.getItem('buttons'))
     let buttonA = buttons.length > 0 ? buttons : buttonArray
@@ -79,15 +80,17 @@ const init = () => {
 
     document.querySelectorAll('.button').forEach(button => {
         button.addEventListener('click', event => {
+            const buttons = JSON.parse(localStorage.getItem('buttons'))
+            let obj = buttons.find(a => a.name == event.target.innerText)
             query_selector_handling(".update-menu").classList.remove("hidden")
             query_selector_handling(".update-menu").classList.add('showB')
             query_selector_handling(".add-menu").classList.remove("showB")
             query_selector_handling(".add-menu").classList.add('hidden')
-            document.getElementById('nameU').value = event.target.innerText;
-            document.getElementById('priceU').value = event.target.value;
 
-            // priceArray.push({ "item": event.target.innerText, "price": event.target.value })
-            // displayItemList()
+            document.getElementById('nameU').value = obj.name;
+            document.getElementById('priceU').value = obj.price;
+            document.getElementById('id').value = obj._id;
+
         });
     });
 
@@ -105,34 +108,96 @@ const init = () => {
     //     }
     // })
 
+    document.getElementById('update-menu').addEventListener('click', async () => {
+        const inputObj = {
+            "name": document.getElementById('nameA').value,
+            "price": document.getElementById('priceA').value
+        }
+
+    });
     document.getElementById('add-menu').addEventListener('click', async () => {
-        //     const savedData = JSON.parse(localStorage.getItem('OrderHistory')) || [];
-        //     const totalPrice = priceArray.reduce((sum, item) => sum + Number(item.price), 0);
+
+        const inputObj = {
+            "name": document.getElementById('nameA').value,
+            "price": document.getElementById('priceA').value
+        }
 
 
-        //     const inputObj = {
-        //         date: !checkbox.checked ? new Date().toJSON().slice(0, 10) : datumValjare.value,
-        //         "totalPrice": totalPrice,
-        //         "items": priceArray
-        //     }
-
-        //     savedData.push(inputObj);
-
-        //     const response = await fetch('https://backend-price.netlify.app/.netlify/functions/api/orderhistory', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json', // Sätt rätt Content-Type
-        //         },
-        //         body: JSON.stringify([inputObj]), // Konvertera data till JSON-sträng
-        //     });
-        //     if (response.ok) {
-        //         const orders = await response.json();
-        //         localStorage.setItem('OrderHistory', JSON.stringify(orders));
-        //     } else {
-        //         console.error('Något gick fel');
-        //     }
+        const response = await fetch('https://backend-price.netlify.app/.netlify/functions/api/button', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', // Sätt rätt Content-Type
+            },
+            body: JSON.stringify([inputObj]), // Konvertera data till JSON-sträng
+        });
+        if (response.ok) {
+            const buttons = await response.json();
+            localStorage.removeItem("buttons");
+            localStorage.setItem('buttons', JSON.stringify(buttons));
+        } else {
+            console.error('Något gick fel');
+        }
         //     priceArray = []
-        //     displayItemList()
+        displayButtons()
+    });
+
+    document.getElementById('update-menu').addEventListener('click', async () => {
+        const id = document.getElementById('id').value
+        const inputObj = {
+            "name": document.getElementById('nameU').value,
+            "price": document.getElementById('priceU').value
+        }
+
+        const response = await fetch(`https://backend-price.netlify.app/.netlify/functions/api/button/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json', // Sätt rätt Content-Type
+            },
+            body: JSON.stringify(inputObj), // Konvertera data till JSON-sträng
+        });
+        if (response.ok) {
+            const buttons = await response.json();
+            localStorage.removeItem("buttons");
+            localStorage.setItem('buttons', JSON.stringify(buttons));
+        } else {
+            console.error('Något gick fel');
+        }
+    });
+
+
+    document.getElementById('delete-menu').addEventListener('click', async () => {
+        const id = document.getElementById('id').value
+        const inputObj = {
+            "name": document.getElementById('nameU').value,
+            "price": document.getElementById('priceU').value
+        }
+
+        const response = await fetch(`https://backend-price.netlify.app/.netlify/functions/api/button/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json', // Sätt rätt Content-Type
+            },
+            body: JSON.stringify(inputObj), // Konvertera data till JSON-sträng
+        });
+        if (response.ok) {
+            const buttons = await response.json();
+            localStorage.removeItem("buttons");
+            localStorage.setItem('buttons', JSON.stringify(buttons));
+        } else {
+            console.error('Något gick fel');
+        }
+    });
+
+
+    document.addEventListener('click', function (event) {
+        // Kolla om klicket inte var på en knapp eller input-fält
+        if (event.target.tagName !== 'BUTTON' && event.target.tagName !== 'INPUT') {
+            // Här kan du lägga till din egen logik
+            query_selector_handling(".add-menu").classList.remove("hidden")
+            query_selector_handling(".add-menu").classList.add('showB')
+            query_selector_handling(".update-menu").classList.remove("showB")
+            query_selector_handling(".update-menu").classList.add('hidden')
+        }
     });
 
     // paymentElement.addEventListener('input', function () {
